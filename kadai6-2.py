28ed24e6ade9600161ddf3256841061e484a5ffa
j22119川野直弘 <j22119ka@edu.tuis.ac.jp>
2025-06-15 21:58:48 +0900

    add kadai6-2.pyコメント追加
    1行目から15行目にコメントを追加しました。

--- kadai6-2.py
+++ kadai6-2.py
@@ -1,3 +1,20 @@
+"""
+参照したオープンデータ そらまめ君(環境省大気汚染物質広域監視システム)
+
+エンドポイント https://soramame.env.go.jp/soramame/api/data_search
+
+機能
+観測データの検索
+地域・測定局コードによる指定
+年月範囲の指定
+
+使用方法
+GETリクエストを送信して、JSON形式でデータを取得。
+パラメータに年月、都道府県、測定局、データ種別を設定。
+
+"""
+
+
 import requests
 import pandas as pd
 import json
@@ -5,11 +22,11 @@ import json
 url = "https://soramame.env.go.jp/soramame/api/data_search"
 
 params = {
-    "Start_YM": "202504", 
-    "End_YM": "202505",
-    "TDFKN_CD": "08",
-    "SKT_CD": "08406050",
-    "REQUEST_DATA": "TEMP", 
+    "Start_YM": "202504", #開始年度
+    "End_YM": "202505", #終了年度
+    "TDFKN_CD": "08", #都道府県の設定(茨城県)
+    "SKT_CD": "08406050", #詳しい地点(地点は神栖消防)
+    "REQUEST_DATA": "TEMP", #ほしいデータ(例 気温)
     "lang": "J",  # 日本語を指定
 }
 
